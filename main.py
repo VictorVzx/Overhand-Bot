5f24b5890bcdb43cef5a867a75b0053b8131d168
Victor Compertino <gifpa45we@gmail.com>
2025-11-21 20:21:26 -0300

    Adicionar atividade ao bot

--- main.py
+++ main.py
@@ -55,6 +55,12 @@ async def dono(ctx):
     usuario = ctx.author.display_name
     print(f"{usuario} usou .dono")
 
+@bot.event()
+async def on_ready():
+    activity = discord.Game(name="Miando incessantemente")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+
 @bot.command()
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, membro: discord.Member, *, motivo="Nenhum motivo informado"):
